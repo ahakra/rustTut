8fb0bc9352b7772b7fa0ab6ac6e0394c63109eaf
Ahmad Akra <ahakra@vanrise.com>
2024-01-12 18:12:49 +0200

    starting http lib

--- http/src/lib.rs
+++ http/src/lib.rs
@@ -1,14 +1,2 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//This tells the compiler that we are creating a new publicly accessible module called httprequest
+pub mod httprequest;
